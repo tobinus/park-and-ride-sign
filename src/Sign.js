9133765170cb434ab6d543b870b8f70f6b0d30ef
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-03-07 09:29:23 +0100

    Customize react to project

---
+++ src/Sign.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Sign.css';
+
+class Sign extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Here be sign!</p>
+      </div>
+    );
+  }
+}
+
+export default Sign;
