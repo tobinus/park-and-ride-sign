b439794150784be9be1f03063c84ba281f1986f9
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-03-07 10:22:51 +0100

    Add ParkInfo

---
+++ src/ParkInfo/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './ParkInfo.css';
+import parkingIcon from './parking.gif';
+
+export default function ParkInfo({parkingSpaces}) {
+  let counter;
+  let text;
+  let textClass;
+
+  if (parkingSpaces === 0) {
+    counter = '  ';
+    text = 'Fullt';
+    textClass = styles.occupied;
+  } else {
+    // Pad with spaces at start of counter text
+    counter = ('  ' + parkingSpaces).slice(-3);
+    text = 'Ledig';
+    textClass = styles.vacant;
+  }
+
+  return <div className={styles['park-info']}>
+    <img src={parkingIcon} alt="Parkering"/>
+    <span className={styles['num-spaces']}>{counter}</span>
+    <span className={textClass}>{text}</span>
+  </div>;
+}
--- src/Sign.js
+++ src/Sign.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './Sign.css';
 import Header from './Header';
+import ParkInfo from './ParkInfo';
 
 class Sign extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header /><ParkInfo parkingSpaces={36}/>
       </div>
     );
   }
